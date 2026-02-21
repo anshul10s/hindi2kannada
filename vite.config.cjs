f7718e45dd1f82737442eb15dbe0e838ab31b839
anshul10s <anshul10s@gmail.com>
2026-02-21 05:12:45 +0000

    firebase compatible deployment

--- vite.config.cjs
+++ vite.config.cjs
@@ -1,7 +1,5 @@
 const { defineConfig } = require('vite');
 
 module.exports = defineConfig(({ command }) => ({
-  // Use the repo name as base only for production builds (GitHub Pages).
-  // Keep `/` as base during `vite` dev for previews and local dev server.
-  base: command === 'build' ? '/hindi2kannada/' : '/',
+  base: '/'
 }));
